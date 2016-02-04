7a4786d059437b0d44134b6082a6293258268f7a
chihoyeung <chiho.yeung@powerdms.com>
2016-02-04 17:21:52 -0500

    missing schema test, test will fail for now but is fine

---
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Create/with_missing_schema.cs
@@ -0,0 +1,58 @@
+namespace Owin.Scim.Tests.Integration.Users.Create
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+
+    using Machine.Specifications;
+
+    using Model;
+    using Model.Users;
+
+    public class with_missing_schema : using_a_scim_server
+    {
+        Establish context = () =>
+        {
+            UserDto = new UserWithNoSchema()
+            {
+                UserName = "Oops"
+            };
+        };
+
+        Because of = () =>
+        {
+            Response = Server
+                .HttpClient
+                .PostAsync("users", new ObjectContent<UserWithNoSchema>(UserDto, new ScimJsonMediaTypeFormatter()))
+                .Result;
+
+            StatusCode = Response.StatusCode;
+
+            Error = StatusCode != HttpStatusCode.BadRequest ? null : Response.Content
+                .ReadAsAsync<IEnumerable<ScimError>>(ScimJsonMediaTypeFormatter.AsArray())
+                .Result
+                .Single();
+        };
+
+        It should_return_bad_request = () => StatusCode.ShouldEqual(HttpStatusCode.BadRequest);
+
+        It should_return_invalid_value = () => Error?.ScimErrorType.ShouldEqual(ScimErrorType.InvalidValue);
+
+        It should_return_error_schema = () => Error?.Schemas.ShouldContain(ScimConstants.Messages.Error);
+
+        protected static UserWithNoSchema UserDto;
+
+        protected static HttpResponseMessage Response;
+
+        protected static HttpStatusCode StatusCode;
+
+        protected static ScimError Error;
+
+        public class UserWithNoSchema
+        {
+            public string UserName { get; set; }
+            public string ExternalId { get; set; }
+        }
+    }
+}
