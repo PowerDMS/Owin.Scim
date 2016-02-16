015f3124ce5f466d28dd9bc173de0ae122834d07
chihoyeung <chiho.yeung@powerdms.com>
2016-02-16 09:40:56 -0500

    added multi attribute and enterprise user tests

---
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Create/with_a_fully_populated_multi_attributes_user.cs
@@ -0,0 +1,157 @@
+﻿namespace Owin.Scim.Tests.Integration.Users.Create
+{
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+
+    using Newtonsoft.Json;
+
+    using Machine.Specifications;
+
+    /// <summary>
+    /// What gets popu
+    /// </summary>
+    public class with_a_fully_populated_multi_attributes_user : using_a_scim_server
+    {
+        Establish context = () =>
+        {
+            UserDto = new MyUser
+            {
+                // older un-supported schema
+                Schemas = new[] { @"urn:ietf:params:scim:schemas:core:2.0:User" },
+                UserName = "Oops",
+                Emails = new[]
+                {
+                    new MyMultiAttribute { Value = "My@BAD.net", Display="Ma Bad", Primary=true},
+                    new MyMultiAttribute { Value = "My@BAD.net", Type = "same email"}
+                },
+                PhoneNumbers = new[]
+                {
+                    new MyMultiAttribute {Value="800123555", Display = "Here is my number", Type="toll-free" },
+                    new MyMultiAttribute {Value="(407) 123-5555", Display = "so call me maybe", Type="local" },
+                    new MyMultiAttribute {Value="1.407.123.5555", Type="local", Primary = false}
+                },
+                Ims = new[]
+                {
+                    new MyMultiAttribute {Value = "ICQ", Display = "Icq"},
+                    new MyMultiAttribute {Value = "fb", Display = "facebook", Type = "personal"}
+                },
+                Photos = new[]
+                {
+                    new MyMultiAttribute {Value = "http://WeirdPhoto/23", Display = "Icq"}
+                }
+            };
+        };
+
+        Because of = () =>
+        {
+            Response = Server
+                .HttpClient
+                .PostAsync("users", new ObjectContent<MyUser>(UserDto, new ScimJsonMediaTypeFormatter
+                {
+                    SerializerSettings = { NullValueHandling = NullValueHandling.Ignore }
+                }))
+                .Result;
+
+            StatusCode = Response.StatusCode;
+
+            if (StatusCode == HttpStatusCode.Created)
+            {
+                var responseBody = Response.Content.ReadAsStringAsync().Result;
+                CreatedUser = JsonConvert.DeserializeObject<MyUser>(responseBody);
+            }
+
+            Error = StatusCode != HttpStatusCode.BadRequest ? null : Response.Content
+                .ReadAsAsync<System.Collections.Generic.IEnumerable<Model.ScimError>>(ScimJsonMediaTypeFormatter.AsArray())
+                .Result
+                .Single();
+        };
+
+        It should_return_created = () => Response.StatusCode.ShouldEqual(HttpStatusCode.Created);
+
+        /// <summary>
+        /// Note on echoing values: per protocol, service provider is required to echo a full saved state of the resource
+        /// However, it is up to the service provider to decide what values to ignore, e.g. you might send multiple addresses
+        /// but the service provider might just save one, or it might ignore display or type attributes.
+        /// </summary>
+        It should_cannonize_email_values = () =>
+        {
+            CreatedUser.ShouldNotBeNull();
+            CreatedUser.Emails.Length.ShouldEqual(UserDto.Emails.Length);
+            for (int i = 0; i < CreatedUser.Emails.Length; i++)
+            {
+                CreatedUser.Emails[i].Value.ShouldEqual(UserDto.Emails[i].Value);
+                CreatedUser.Emails[i].Display.ShouldBeEqualIgnoringCase(UserDto.Emails[i].Value);
+                CreatedUser.Emails[i].Type.ShouldEqual(UserDto.Emails[i].Type);
+                CreatedUser.Emails[i].Primary.ShouldEqual(UserDto.Emails[i].Primary ?? false);
+            }
+        };
+
+        It should_cannonize_phone_values = () =>
+        {
+            CreatedUser.ShouldNotBeNull();
+            CreatedUser.PhoneNumbers.Length.ShouldEqual(UserDto.PhoneNumbers.Length);
+            for (int i = 0; i < CreatedUser.PhoneNumbers.Length; i++)
+            {
+                CreatedUser.PhoneNumbers[i].Value.ShouldEqual(UserDto.PhoneNumbers[i].Value);
+                UserDto.PhoneNumbers[i].Value.ToCharArray()
+                    .ShouldContain(CreatedUser.PhoneNumbers[i].Display.ToCharArray());
+                CreatedUser.PhoneNumbers[i].Type.ShouldEqual(UserDto.PhoneNumbers[i].Type);
+                CreatedUser.PhoneNumbers[i].Primary.ShouldEqual(UserDto.PhoneNumbers[i].Primary ?? false);
+            }
+        };
+
+        protected static MyUser UserDto;
+
+        protected static MyUser CreatedUser;
+
+        protected static HttpResponseMessage Response;
+
+        protected static HttpStatusCode StatusCode;
+
+        protected static Model.ScimError Error;
+
+        /// <summary>
+        /// Using my own user class to test interoperativity and not favor our own User class
+        /// </summary>
+        public class MyUser
+        {
+            public string[] Schemas { get; set; }
+            public string UserName { get; set; }
+
+            public MyMultiAttribute[] Emails { get; set; }
+
+            public MyMultiAttribute[] PhoneNumbers { get; set; }
+
+            public MyMultiAttribute[] Ims { get; set; }
+
+            public MyMultiAttribute[] Photos { get; set; }
+        }
+
+        public class MyMultiAttribute
+        {
+            public string Value;
+            public string Display;
+            public string Type;
+            public bool? Primary;
+        }
+
+        public class MyMultiAttribute2
+        {
+            public string Value;
+            public string Display;
+            public string Type;
+        }
+
+        public class MyMultiAttribute3
+        {
+            public string Value;
+            public string Display;
+        }
+
+        public class MyMultiAttribute4
+        {
+            public string Value;
+        }
+    }
+}
--- source/_tests/Owin.Scim.Tests/Integration/Users/Create/with_a_fully_populated_simple_attributes_user.cs
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Create/with_a_fully_populated_simple_attributes_user.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Owin.Scim.Tests.Integration.Users.Create
 {
     using System.Net;
@@ -7,7 +9,10 @@ namespace Owin.Scim.Tests.Integration.Users.Create
     using Model.Users;
 
     using Ploeh.AutoFixture;
-    
+
+    /// <summary>
+    /// What gets popu
+    /// </summary>
     public class with_a_fully_populated_simple_attributes_user : when_creating_a_user
     {
         Establish context = () =>
@@ -20,7 +25,6 @@ namespace Owin.Scim.Tests.Integration.Users.Create
                 .With(x => x.PreferredLanguage, "en-US,en,es")
                 .With(x => x.Locale, "en-US")
                 .With(x => x.Timezone, @"US/Eastern")
-                .With(x => x.ProfileUrl, null)
                 .With(x => x.Emails, null)
                 .With(x => x.PhoneNumbers, null)
                 .With(x => x.Ims, null)
@@ -35,20 +39,22 @@ namespace Owin.Scim.Tests.Integration.Users.Create
 
         It should_return_the_user = () => CreatedUser.Id.ShouldNotBeEmpty();
 
-        It should_return_meta = () =>
+        It should_return_null_password = () => CreatedUser.Password.ShouldBeNull();
+
+        private It should_return_meta = () =>
         {
             CreatedUser.Meta.ShouldNotBeNull();
             CreatedUser.Meta.ResourceType.ShouldEqual("User");
             CreatedUser.Meta.Created.ShouldNotBeNull();
             CreatedUser.Meta.LastModified.ShouldEqual(CreatedUser.Meta.Created);
             CreatedUser.Meta.Location.ShouldNotBeNull();
-            CreatedUser.Meta.Version.ShouldNotBeNull();
         };
 
         It should_echo_create_values = () =>
         {
             CreatedUser.Schemas.ShouldEqual(UserDto.Schemas);
             CreatedUser.UserName.ShouldEqual(UserDto.UserName);
+            CreatedUser.ExternalId.ShouldEqual(UserDto.ExternalId);
             CreatedUser.DisplayName.ShouldEqual(UserDto.DisplayName);
             CreatedUser.NickName.ShouldEqual(UserDto.NickName);
             CreatedUser.ProfileUrl.ShouldEqual(UserDto.ProfileUrl);
@@ -58,7 +64,13 @@ namespace Owin.Scim.Tests.Integration.Users.Create
             CreatedUser.Locale.ShouldEqual(UserDto.Locale);
             CreatedUser.Timezone.ShouldEqual(UserDto.Timezone);
             CreatedUser.Active.ShouldEqual(UserDto.Active);
-            CreatedUser.Password.ShouldBeNull();
+
+            CreatedUser.Name.GivenName.ShouldEqual(UserDto.Name.GivenName);
+            CreatedUser.Name.MiddleName.ShouldEqual(UserDto.Name.MiddleName);
+            CreatedUser.Name.FamilyName.ShouldEqual(UserDto.Name.FamilyName);
+            CreatedUser.Name.Formatted.ShouldEqual(UserDto.Name.Formatted);
+            CreatedUser.Name.HonorificPrefix.ShouldEqual(UserDto.Name.HonorificPrefix);
+            CreatedUser.Name.HonorificSuffix.ShouldEqual(UserDto.Name.HonorificSuffix);
         };
     }
 }
