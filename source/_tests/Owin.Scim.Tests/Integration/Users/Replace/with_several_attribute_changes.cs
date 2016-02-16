8cfa3bda757979f784e5780e637a8952b331ef5c
chihoyeung <chiho.yeung@powerdms.com>
2016-02-16 14:50:21 -0500

    added retrieve and replace tests

---
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Replace/with_several_attribute_changes.cs
@@ -0,0 +1,80 @@
+﻿namespace Owin.Scim.Tests.Integration.Users.Replace
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Formatting;
+
+    using Machine.Specifications;
+
+    using Model.Users;
+
+    using Ploeh.AutoFixture;
+
+    public class with_several_attribute_changes : using_a_scim_server
+    {
+        Establish context = () =>
+        {
+            var autoFixture = new Fixture();
+
+            var existingUser = autoFixture.Build<EnterpriseUser>()
+                .With(x => x.UserName, UserNameUtility.GenerateUserName())
+                .With(x => x.Password, "somePass")
+                .With(x => x.PreferredLanguage, "en-US,en,es")
+                .With(x => x.Locale, "en-US")
+                .With(x => x.Timezone, @"US/Eastern")
+                .With(x => x.Emails, null)
+                .With(x => x.PhoneNumbers, null)
+                .With(x => x.Ims, null)
+                .With(x => x.Photos, null)
+                .With(x => x.Addresses, null)
+                .Create();
+
+
+            // Insert the first user so there's one already in-memory.
+            var userRecord = Server
+                .HttpClient
+                .PostAsync("users", new ObjectContent<EnterpriseUser>(existingUser, new ScimJsonMediaTypeFormatter()))
+                .Result;
+
+            UserDto = userRecord.Content.ReadAsAsync<EnterpriseUser>(ScimJsonMediaTypeFormatter.AsArray()).Result;
+
+            UserId = UserDto.Id;
+
+            // Common use case for replace is for client to send the full object back
+            UserDto.UserName = UserNameUtility.GenerateUserName();
+            UserDto.Password = "someOtherPass";
+            UserDto.Enterprise.EmployeeNumber = "007";
+        };
+
+        Because of = async () =>
+        {
+            Response = await Server
+                .HttpClient
+                .PutAsync("users/" + UserId, new ObjectContent<EnterpriseUser>(UserDto, new ScimJsonMediaTypeFormatter()))
+                .AwaitResponse()
+                .AsTask;
+
+            if (Response.StatusCode == HttpStatusCode.OK)
+            {
+                UpdatedUser = Response.Content.ReadAsAsync<EnterpriseUser>(ScimJsonMediaTypeFormatter.AsArray()).Result;
+            }
+        };
+
+        It should_contain_the_new_values = () =>
+        {
+            Response.StatusCode.ShouldEqual(HttpStatusCode.OK);
+            UpdatedUser.UserName.ShouldEqual(UserDto.UserName);
+            UpdatedUser.Enterprise.EmployeeNumber.ShouldEqual(UserDto.Enterprise.EmployeeNumber);
+        };
+
+        It should_not_return_password = () => UpdatedUser.Password.ShouldBeNull();
+
+        protected static EnterpriseUser UserDto;
+
+        protected static EnterpriseUser UpdatedUser;
+
+        protected static string UserId;
+
+        protected static HttpResponseMessage Response;
+    }
+}
---
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Retrieve/when_retrieving_fully_populated_user.cs
@@ -0,0 +1,68 @@
+﻿namespace Owin.Scim.Tests.Integration.Users.Retrieve
+{
+    using System.Net;
+    using System.Net.Http;
+
+    using Machine.Specifications;
+
+    using Model.Users;
+
+    using Ploeh.AutoFixture;
+
+    public class when_retrieving_fully_populated_user : using_a_scim_server
+    {
+        Because of = () =>
+        {
+            var autoFixture = new Fixture();
+
+            var existingUser = autoFixture.Build<EnterpriseUser>()
+                .With(x => x.UserName, UserNameUtility.GenerateUserName())
+                .With(x => x.Password, "somePass")
+                .With(x => x.PreferredLanguage, "en-US,en,es")
+                .With(x => x.Locale, "en-US")
+                .With(x => x.Timezone, @"US/Eastern")
+                .With(x => x.Emails, null)
+                .With(x => x.PhoneNumbers, null)
+                .With(x => x.Ims, null)
+                .With(x => x.Photos, null)
+                .With(x => x.Addresses, null)
+                .Create();
+
+            // Insert the first user so there's one already in-memory.
+            Response = Server
+                .HttpClient
+                .PostAsync("users", new ObjectContent<EnterpriseUser>(existingUser, new ScimJsonMediaTypeFormatter()))
+                .Result;
+
+            if (Response.StatusCode == HttpStatusCode.Created)
+            {
+                JsonData = Response.Content.ReadAsStringAsync().Result;
+                User = Newtonsoft.Json.JsonConvert.DeserializeObject<EnterpriseUser>(JsonData);
+            }
+
+            var client = Server.HttpClient;
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/scim+json"));
+
+            Response = Server
+                .HttpClient
+                .GetAsync("users/" + User.Id)
+                .Result;
+        };
+
+        private It should_return_same_data_echoed_during_create = () =>
+        {
+            Response.StatusCode.ShouldEqual(HttpStatusCode.OK);
+            NewJsonData = Response.Content.ReadAsStringAsync().Result;
+
+            JsonData.ShouldEqual(NewJsonData);
+        };
+
+        protected static HttpResponseMessage Response;
+
+        protected static User User;
+
+        protected static string JsonData;
+
+        protected static string NewJsonData;
+    }
+}
