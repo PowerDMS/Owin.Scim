35ec1f5ae546fb6d78b7002c972b27d21b92b355
chihoyeung <chiho.yeung@powerdms.com>
2016-02-25 19:47:07 -0500

    added more tests

---
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Update/remove/with_path_direct_subattribute_no_change.cs
@@ -0,0 +1,46 @@
+namespace Owin.Scim.Tests.Integration.Users.Update.remove
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+
+    using Machine.Specifications;
+
+    using Model.Users;
+
+    public class with_path_direct_subattribute_no_change : when_updating_a_user
+    {
+        Establish context = () =>
+        {
+            UserToUpdate = new User
+            {
+                UserName = UserNameUtility.GenerateUserName(),
+                Name = new Name
+                {
+                    FamilyName = "Smith",
+                    GivenName = "John"
+                }
+            };
+
+            PatchContent = new StringContent(
+                @"
+                    {
+                        ""schemas"": [""urn:ietf:params:scim:api:messages:2.0:PatchOp""],
+                        ""Operations"": [{
+                            ""op"": ""remove"",
+                            ""path"": ""name.formatted""
+                        }]
+                    }",
+                Encoding.UTF8,
+                "application/json");
+        };
+
+        It should_return_ok = () => PatchResponse.StatusCode.ShouldEqual(HttpStatusCode.OK);
+
+        It should_not_change_complex_attribute = () => UpdatedUser.Name.ShouldBeLike(UserToUpdate.Name);
+
+        It should_not_update_version = () => UpdatedUser.Meta.Version.ShouldEqual(UserToUpdate.Meta.Version);
+
+        It should_not_update_last_modified = () => UpdatedUser.Meta.LastModified.ShouldEqual(UserToUpdate.Meta.LastModified);
+    }
+}
---
+++ source/_tests/Owin.Scim.Tests/Integration/Users/Update/replace/with_path_direct_subattribute_no_change.cs
@@ -0,0 +1,47 @@
+namespace Owin.Scim.Tests.Integration.Users.Update.replace
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+
+    using Machine.Specifications;
+
+    using Model.Users;
+
+    public class with_path_direct_subattribute_no_change : when_updating_a_user
+    {
+        Establish context = () =>
+        {
+            UserToUpdate = new User
+            {
+                UserName = UserNameUtility.GenerateUserName(),
+                Name = new Name
+                {
+                    FamilyName = "Smith",
+                    GivenName = "John"
+                }
+            };
+
+            PatchContent = new StringContent(
+                @"
+                    {
+                        ""schemas"": [""urn:ietf:params:scim:api:messages:2.0:PatchOp""],
+                        ""Operations"": [{
+                            ""op"": ""replace"",
+                            ""path"": ""name.givenName"",
+                            ""value"": ""John""
+                        }]
+                    }",
+                Encoding.UTF8,
+                "application/json");
+        };
+
+        It should_return_ok = () => PatchResponse.StatusCode.ShouldEqual(HttpStatusCode.OK);
+
+        It should_not_change_complex_attribute = () => UpdatedUser.Name.ShouldBeLike(UserToUpdate.Name);
+
+        It should_not_update_version = () => UpdatedUser.Meta.Version.ShouldEqual(UserToUpdate.Meta.Version);
+
+        It should_not_update_last_modified = () => UpdatedUser.Meta.LastModified.ShouldEqual(UserToUpdate.Meta.LastModified);
+    }
+}
