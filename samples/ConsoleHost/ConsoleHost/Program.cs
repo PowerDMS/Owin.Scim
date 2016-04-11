29e6b205b127f069342634e9ad9ca62017dc6e7c
Daniel Gioulakis <daniel.gioulakis@powerdms.com>
2016-04-11 16:29:49 +0200

    Fixing issues with nuspec and dependencies.  Adding sample application.

---
+++ samples/ConsoleHost/ConsoleHost/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleHost
+{
+    using System;
+
+    using Owin;
+    using Owin.Scim.Configuration;
+    using Owin.Scim.Extensions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<CompositionRoot>("http://localhost:8080"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    internal class CompositionRoot
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Map("/scim", app =>
+            {
+                app.UseScimServer(
+                    new ScimServerConfiguration { RequireSsl = true }
+                        .AddCompositionConditions(
+                            fileInfo => fileInfo.Name.StartsWith("ConsoleHost", StringComparison.OrdinalIgnoreCase) && 
+                            fileInfo.Extension.Equals(".exe", StringComparison.OrdinalIgnoreCase)));
+            });
+        }
+    }
+}
