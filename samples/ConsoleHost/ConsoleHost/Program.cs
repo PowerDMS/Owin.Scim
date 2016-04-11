660611e877291a75b1221f3014c1653d46935c4c
Daniel Gioulakis <daniel.gioulakis@powerdms.com>
2016-04-11 17:46:03 +0200

    Fixed and updated sample console app.

--- samples/ConsoleHost/ConsoleHost/Program.cs
+++ samples/ConsoleHost/ConsoleHost/Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace ConsoleHost
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
 
     using Owin;
-//    using Owin.Scim.Configuration;
-//    using Owin.Scim.Extensions;
+    using Owin.Scim.Configuration;
+    using Owin.Scim.Extensions;
 
     class Program
     {
@@ -12,7 +14,6 @@
         {
             using (Microsoft.Owin.Hosting.WebApp.Start<CompositionRoot>("http://localhost:8080"))
             {
-                Console.WriteLine("Press [enter] to quit...");
                 Console.ReadLine();
             }
         }
@@ -22,14 +23,17 @@
     {
         public void Configuration(IAppBuilder appBuilder)
         {
-//            appBuilder.Map("/scim", app =>
-//            {
-//                app.UseScimServer(
-//                    new ScimServerConfiguration { RequireSsl = true }
-//                        .AddCompositionConditions(
-//                            fileInfo => fileInfo.Name.StartsWith("ConsoleHost", StringComparison.OrdinalIgnoreCase) && 
-//                            fileInfo.Extension.Equals(".exe", StringComparison.OrdinalIgnoreCase)));
-//            });
+            appBuilder.Map("/scim", app =>
+            {
+                app.UseScimServer(
+                    new ScimServerConfiguration { RequireSsl = false }
+                        .AddCompositionConditions(
+                            fileInfo => fileInfo.Name.StartsWith("ConsoleHost", StringComparison.OrdinalIgnoreCase) && 
+                            fileInfo.Extension.Equals(".exe", StringComparison.OrdinalIgnoreCase)));
+            });
+
+            var address = ((IList<IDictionary<string, object>>)appBuilder.Properties["host.Addresses"])[0];
+            Console.WriteLine(string.Format("SCIM server is listening at: {0}", new UriBuilder(address["scheme"].ToString(), address["host"].ToString(), Convert.ToInt32(address["port"]), address["path"].ToString() + "/scim")));
         }
     }
 }
